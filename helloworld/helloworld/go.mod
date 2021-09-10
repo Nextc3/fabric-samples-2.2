73fcd03a9fd2268a550492fb7653ebd9560b686d
Caio Costa <nextc3@gmail.com>
2021-09-09 21:56:36 -0300

    Começando

---
+++ helloworld/helloworld/go.mod
@@ -0,0 +1,8 @@
+module github.com/Nextc3/helloworld
+
+go 1.16
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/hyperledger/fabric-sdk-go v1.0.0
+)
---
+++ test-network/inicializarRede.sh
@@ -0,0 +1,12 @@
+####
+./desligarRede.sh
+./meuNetwork.sh up
+
+./network.sh up createChannel -ca -s couchdb
+
+#variaveis de ambiente de Org1
+source variaveis_de_ambienteOrg1.sh
+
+
+
+
