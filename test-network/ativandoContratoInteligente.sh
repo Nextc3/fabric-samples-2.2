42989ad90b9a40539e331ecb2d620ea01509a7f7
Caio Costa Cavalcante <nextc3@gmail.com>
2022-05-04 23:49:22 -0300

    Arquivos de scripts para contrato inteligente

---
+++ test-network/ativandoContratoInteligente.sh
@@ -0,0 +1,2 @@
+./meuNetwork.sh deployCC -ccn contratointeligente -ccp ../notificacao-covid-blockchain/chaincode -ccl go
+#./meuNetwork.sh deployCC -ccn helloworld -ccp ../chaincode/helloworld/external/go -ccl go
